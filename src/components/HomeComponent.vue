b40c88db935e14ae6778e228526d5cc3d355330c
AnatolyRM <1neytron@gmail.com>
2022-11-05 16:50:38 +0300

    add project-2

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 class="Home">
+      Home
+    </h1>
+  </div>
+  <br>
+  <div class="counter">
+    <div class="flex justify-content-center">
+      <button @click="decreaseValue">Decrease </button>
+      <h1>{{ counter }}</h1>
+      <button @click="increaseValue">Increase </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+
+const increaseValue = () => counter.value++
+const decreaseValue = () => counter.value--
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import AboutComponent from '@/components/AboutComponent.vue'
+import HomeComponent from '@/components/HomeComponent.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeComponent
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutComponent
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+export default router
