35f76041e23677b88bdc46a58bfc04216d9060ca
AnatolyRM <1neytron@gmail.com>
2022-11-05 14:28:25 +0300

    init: new project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import GithubUserCard from './components/GithubUserCard.vue'
+</script>
+
+<template>
+  <div class="">
+    <github-user-card></github-user-card>
+  </div>
+</template>
+
+<style scoped></style>
