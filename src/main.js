6b806cf16a1aca489f5b662013a418923d9208c0
AnatolyRM <1neytron@gmail.com>
2022-11-06 21:55:00 +0300

    del filles

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-var infiniteScroll =  require('vue-infinite-scroll');
-
-
-createApp(App).use(store).use(router).use(infiniteScroll).mount("#app");
